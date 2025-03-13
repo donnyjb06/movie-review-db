0ebcb636d59780254d8e55bd29eeb48141b54255
DonovanJabbar <djabbar51@gmail.com>
2025-03-13 17:24:56 -0600

    Refactor: Create reusable Container component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,22 @@
 import React from "react";
-import Header from "./components/Header/Header"
+import Header from "./components/Header/Header";
 import { ThemeContextProvider } from "./helpers/providers/ThemeProvider";
 import Main from "./components/Main/Main";
 import Form from "./components/Form/Form";
+import Container from "./components/Container/Container";
+import InputGroup from "./components/Input/InputGroup";
 
 const App: React.FunctionComponent = () => {
-  return(
+  return (
     <ThemeContextProvider>
       <Header />
       <Main>
-        <Form submitBtnText="Submit Review">
-
-        </Form>
+        <Container>
+          <Form submitBtnText="Submit Review"></Form>
+        </Container>
       </Main>
     </ThemeContextProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;

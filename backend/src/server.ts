4ccffefd57f3f4250e7fae54d8b6fbcb65aff447
DonovanJabbar <djabbar51@gmail.com>
2025-03-14 14:26:53 -0600

    Fix: Add cors to server to prevent violations

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import Express from "express";
-import { PORT } from "config/config";
-import { reviewRoutes } from "routes/review.route";
+import { PORT } from "./config/config.js";
+import { reviewRoutes } from "./routes/review.route.js";
+import cors from "cors";
 
 const app = Express();
 
+app.use(cors())
 app.use(Express.json());
 
 app.use("/review", reviewRoutes);

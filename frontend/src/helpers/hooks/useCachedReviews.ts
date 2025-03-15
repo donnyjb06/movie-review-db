1dd15498842c1d4ec57491817c49b5770200db8f
DonovanJabbar <djabbar51@gmail.com>
2025-03-15 15:12:53 -0600

    Fix: Only update cached reviews on successful response

--- frontend/src/helpers/hooks/useCachedReviews.ts
+++ frontend/src/helpers/hooks/useCachedReviews.ts
@@ -2,47 +2,53 @@ import React from "react";
 import { Review } from "../types/review";
 
 export const useCachedReviews = (requestReviews: () => Promise<Review[]>) => {
-  const [cachedReviews, setCachedReviews] = React.useState<Review[]>([] as Review[])
+  const [cachedReviews, setCachedReviews] = React.useState<Review[]>([]);
 
   React.useEffect(() => {
     const fetchReviews = async () => {
-      const reviews = await requestReviews();
-      setCachedReviews(reviews)
-    }
+      try {
+        const reviews = await requestReviews();
+        if (reviews && reviews.length > 0) {
+          setCachedReviews(reviews);
+        }
+      } catch (error) {
+        console.error("Error fetching reviews:", error);
+      }
+    };
 
-    fetchReviews()
-  }, [requestReviews])
+    fetchReviews();
+  }, [requestReviews]);
 
   React.useEffect(() => {
-    updateSessionStorage()
-  }, [cachedReviews])
+    updateSessionStorage();
+  }, [cachedReviews]);
 
   const updateSessionStorage = () => {
-    window.sessionStorage.setItem("reviews", JSON.stringify(cachedReviews))
-  }
+    window.sessionStorage.setItem("reviews", JSON.stringify(cachedReviews));
+  };
 
   const getCachedReviews = () => {
     const reviewsInStorage = window.sessionStorage.getItem("reviews");
-    return reviewsInStorage ? JSON.parse(reviewsInStorage) as Review[] : [] as Review[];
-  }
+    return reviewsInStorage
+      ? (JSON.parse(reviewsInStorage) as Review[])
+      : ([] as Review[]);
+  };
 
   const cacheNewReview = (newReview: Review) => {
-    setCachedReviews( prevCachedReviews => (
-      [
-        ...prevCachedReviews,
-        newReview
-      ]
-    ))
-  }
-
+    setCachedReviews((prevCachedReviews) => [...prevCachedReviews, newReview]);
+  };
 
   const deleteReviewFromCache = (id: string) => {
-    const index = cachedReviews.findIndex( review => review.id === id);
+    const index = cachedReviews.findIndex((review) => review.id === id);
 
-    const newCachedReviews = cachedReviews.splice(index, 1)
+    const newCachedReviews = cachedReviews.splice(index, 1);
     setCachedReviews(newCachedReviews);
-  }
+  };
 
-  return [getCachedReviews, cacheNewReview, deleteReviewFromCache, cachedReviews] as const;
-}
-
+  return [
+    getCachedReviews,
+    cacheNewReview,
+    deleteReviewFromCache,
+    cachedReviews,
+  ] as const;
+};
